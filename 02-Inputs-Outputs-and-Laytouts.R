ff93e4414e9e67e0f1bf138781983cf742cb307f
Vitor Borges Jr <Vitor>
2020-11-20 18:16:05 -0300

    Create 02-Inputs-Outputs-and-Laytouts.R

---
+++ 02-Inputs-Outputs-and-Laytouts.R
@@ -0,0 +1,16 @@
+# -------------------------------------------------
+# Building Web Applications with Shine in R - Inputs, Outputs, and Layouts
+# 20 nov 2020
+# VNTBJR
+# ------------------------------------------------
+# 
+# Inputs ------------------------------------------------
+# Exemples of input
+# textInput() - allow users to input text that was utilized in the server to 
+# update the graph
+# sliderInput() - allow users to select a value of a variable using a slide selector 
+# selectInput() - allows for a selection from a list of fixed options
+# numericalInput() - allows to provide a range of numbers users can choose from,
+# which they can increase or decrease using the little arrows
+# dateRangeInput() - allows to provide users with a set of dates, and a calendar 
+# dropdown appears when they click so they can select a specific one
