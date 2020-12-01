640f948d019a4eccd359125e794edf472f72e011
Vitor Borges Jr <vntborgesjr@gmail.com>
2020-12-01 09:57:49 -0300

    Update 01-Shiny-review.R

--- Case-Studies-Building-Web-Applications-with-Shiny/01-Shiny-review.R
+++ Case-Studies-Building-Web-Applications-with-Shiny/01-Shiny-review.R
@@ -206,3 +206,34 @@ server <- function(input, output) {
 
 # Run the application
 shinyApp(ui = ui, server = server)
+
+# Reactivity 101  -------------------------------------------
+# Reactive contexts
+ui <- fluidPage(
+  numericInput("num1", "Number 1", 5),
+  numericInput("num2", "Number 2", 10),
+  textOutput("result")
+)
+
+server <- function(input, output) {
+  # Calculate the sum of the inputs
+  my_sum <- reactive({
+    input$num1 + input$num2
+  })
+  
+  # Calculate the average of the inputs
+  my_average <- reactive({
+    my_sum() / 2
+  })
+  
+  output$result <- renderText({
+    paste(
+      # Print the calculated sum
+      "The sum is", my_sum(),
+      # Print the calculated average
+      "and the average is", my_average()
+    )
+  })
+}
+
+shinyApp(ui, server)
