65fdd68575e5e79f1aba682a74eae58eef265e8b
Vitor Borges Jr <vntborgesjr@gmail.com>
2020-12-02 06:16:57 -0300

    Update 02-Make-the-perfect-plot-using-Shiny.R

--- Case-Studies-Building-Web-Applications-with-Shiny/02-Make-the-perfect-plot-using-Shiny.R
+++ Case-Studies-Building-Web-Applications-with-Shiny/02-Make-the-perfect-plot-using-Shiny.R
@@ -127,3 +127,269 @@ server <- function(input, output) {
 shinyApp(ui = ui, server = server)
 
 # More input types  -------------------------------------------
+# Add colours to your plot: radio buttons
+# Define UI for the application
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      textInput("title", "Title", "GDP vs life exp"),
+      numericInput("size", "Point size", 1, 1),
+      checkboxInput("fit", "Add line of best fit", FALSE),
+      # Add radio buttons for colour
+      radioButtons("color", 'Point color', c('blue', 'red', 'green', 'black'))
+    ),
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
+
+# Define the server logic
+server <- function(input, output) {
+  output$plot <- renderPlot({
+    p <- ggplot(gapminder, aes(gdpPercap, lifeExp)) +
+      # Use the value of the color input as the point colour
+      geom_point(size = input$size, col = input$color) +
+      scale_x_log10() +
+      ggtitle(input$title)
+    
+    if (input$fit) {
+      p <- p + geom_smooth(method = "lm")
+    }
+    p
+  })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
+
+# Add a continent selector: select input
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      textInput("title", "Title", "GDP vs life exp"),
+      numericInput("size", "Point size", 1, 1),
+      checkboxInput("fit", "Add line of best fit", FALSE),
+      radioButtons("color", "Point color",
+                   choices = c("blue", "red", "green", "black")),
+      # Add a continent dropdown selector
+      selectInput('continents', 'Continents',
+             choices = levels(factor(gapminder$continent)),
+             multiple = TRUE,
+             selected = 'Europe')
+    ),
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
+
+# Define the server logic
+server <- function(input, output) {
+  output$plot <- renderPlot({
+    # Subset the gapminder dataset by the chosen continents
+    data <- subset(gapminder,
+                   continent %in% input$continents)
+    
+    p <- ggplot(data, aes(gdpPercap, lifeExp)) +
+      geom_point(size = input$size, col = input$color) +
+      scale_x_log10() +
+      ggtitle(input$title)
+    
+    if (input$fit) {
+      p <- p + geom_smooth(method = "lm")
+    }
+    p
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
+# Add a year filter: numeric slider input
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      textInput("title", "Title", "GDP vs life exp"),
+      numericInput("size", "Point size", 1, 1),
+      checkboxInput("fit", "Add line of best fit", FALSE),
+      radioButtons("color", "Point color",
+                   choices = c("blue", "red", "green", "black")),
+      selectInput("continents", "Continents",
+                  choices = levels(gapminder$continent),
+                  multiple = TRUE,
+                  selected = "Europe"),
+      # Add a slider selector for years to filter
+      sliderInput(inputId = "years", label = 'Years', 
+                  min = min(gapminder$year), max = max(gapminder$year), 
+                  value = c(1977, 2002))
+    ),
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
+
+# Define the server logic
+server <- function(input, output) {
+  output$plot <- renderPlot({
+    # Subset the gapminder data by the chosen years
+    data <- subset(gapminder,
+                   continent %in% input$continents &
+                     year >= input$years[1] & year <= input$years[2])
+    
+    p <- ggplot(data, aes(gdpPercap, lifeExp)) +
+      geom_point(size = input$size, col = input$color) +
+      scale_x_log10() +
+      ggtitle(input$title)
+    
+    if (input$fit) {
+      p <- p + geom_smooth(method = "lm")
+    }
+    p
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
+# Add colours to your plot:color input
+# Load the colourpicker package
+library(colourpicker)
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      textInput("title", "Title", "GDP vs life exp"),
+      numericInput("size", "Point size", 1, 1),
+      checkboxInput("fit", "Add line of best fit", FALSE),
+      
+      # Replace the radio buttons with a color input
+      colourInput(inputId = 'color', label = 'Point color', value = 'blue'),
+      selectInput("continents", "Continents",
+                  choices = levels(gapminder$continent),
+                  multiple = TRUE,
+                  selected = "Europe"),
+      sliderInput("years", "Years",
+                  min(gapminder$year), max(gapminder$year),
+                  value = c(1977, 2002))
+    ),
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
+
+# Define the server logic
+server <- function(input, output) {
+  output$plot <- renderPlot({
+    data <- subset(gapminder,
+                   continent %in% input$continents &
+                     year >= input$years[1] & year <= input$years[2])
+    
+    p <- ggplot(data, aes(gdpPercap, lifeExp)) +
+      geom_point(size = input$size, col = input$color) +
+      scale_x_log10() +
+      ggtitle(input$title)
+    
+    if (input$fit) {
+      p <- p + geom_smooth(method = "lm")
+    }
+    p
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
+# Making tour plot larger
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      textInput("title", "Title", "GDP vs life exp"),
+      numericInput("size", "Point size", 1, 1),
+      checkboxInput("fit", "Add line of best fit", FALSE),
+      colourInput("color", "Point color", value = "blue"),
+      selectInput("continents", "Continents",
+                  choices = levels(gapminder$continent),
+                  multiple = TRUE,
+                  selected = "Europe"),
+      sliderInput("years", "Years",
+                  min(gapminder$year), max(gapminder$year),
+                  value = c(1977, 2002))
+    ),
+    mainPanel(
+      # Make the plot 600 pixels wide and 600 pixels tall
+      plotOutput("plot", width = 600, height = 600)
+    )
+  )
+)
+
+# Define the server logic
+server <- function(input, output) {
+  output$plot <- renderPlot({
+    data <- subset(gapminder,
+                   continent %in% input$continents &
+                     year >= input$years[1] & year <= input$years[2])
+    
+    p <- ggplot(data, aes(gdpPercap, lifeExp)) +
+      geom_point(size = input$size, col = input$color) +
+      scale_x_log10() +
+      ggtitle(input$title)
+    
+    if (input$fit) {
+      p <- p + geom_smooth(method = "lm")
+    }
+    p
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
+# Make your plot interactive
+# Load the plotly package
+library(plotly)
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      textInput("title", "Title", "GDP vs life exp"),
+      numericInput("size", "Point size", 1, 1),
+      checkboxInput("fit", "Add line of best fit", FALSE),
+      colourInput("color", "Point color", value = "blue"),
+      selectInput("continents", "Continents",
+                  choices = levels(gapminder$continent),
+                  multiple = TRUE,
+                  selected = "Europe"),
+      sliderInput("years", "Years",
+                  min(gapminder$year), max(gapminder$year),
+                  value = c(1977, 2002))
+    ),
+    mainPanel(
+      # Replace the `plotOutput()` with the plotly version
+      plotlyOutput("plot")
+    )
+  )
+)
+
+# Define the server logic
+server <- function(input, output) {
+  # Replace the `renderPlot()` with the plotly version
+  output$plot <- renderPlotly({
+    # Convert the existing ggplot2 to a plotly plot
+    ggplotly({
+      data <- subset(gapminder,
+                     continent %in% input$continents &
+                       year >= input$years[1] & year <= input$years[2])
+      
+      p <- ggplot(data, aes(gdpPercap, lifeExp)) +
+        geom_point(size = input$size, col = input$color) +
+        scale_x_log10() +
+        ggtitle(input$title)
+      
+      if (input$fit) {
+        p <- p + geom_smooth(method = "lm")
+      }
+      p
+    })
+  })
+}
+
+shinyApp(ui = ui, server = server)
